8e430869e8645fecac677581780efd504bb7ce7f
Stoeffel <schtoeffel@gmail.com>
2016-04-26 20:32:25 +0200

    fixes component test
    closes #15

--- tests/components/title.test.js
+++ tests/components/title.test.js
@@ -4,7 +4,16 @@ import { shallow } from 'enzyme'
 import { h } from '../../src/helpers/h'
 import Title from '../../src/components/title'
 
+const { h1 } = h
+
 test('title component', t => {
-  const wrapper = shallow(Title({ text: 'test' }))
-  t.truthy(wrapper.contains(h.h1({ style: { color: 'red' } }, 'test')))
+  const notLoading = shallow(Title({ text: 'test' }))
+  t.truthy(notLoading.contains(
+    h1({ style: { color: 'red' }, className: 'title' }, 'test')
+  ))
+
+  const loading = shallow(Title({ text: 'test', loading: true }))
+  t.truthy(loading.contains(
+    h1({ style: { color: 'red' }, className: 'loading title' }, 'test')
+  ))
 })
